0ba1c35a3d92a4fb7302a252871fb51f3f83aaaa
Victoria Bright <victoriacbright92@gmail.com>
2022-11-01 09:17:12 -0500

    complete

---
+++ script.js
@@ -0,0 +1,61 @@
+//1a
+ const jPDinos =  [
+    'Velociraptors',
+    `Tyrannosaurus Rex`,
+    `Dilophosaurus`
+ ];
+
+ //1b 
+ function seeDinos(a, ...b) {
+    console.log(a);
+    console.log(b);
+ }
+seeDinos('Velociraptors', `Tyrannosaurus Rex`, `Dilophosaurus`);
+
+
+//2a
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+//2b
+
+seeCharacters = ({ellieSattler}) => {
+    console.log(ellieSattler);
+}
+seeCharacters(jPCharacters);
+
+//BONUS
+
+//3a
+const jurassicParkMovies = {
+    first:{ 
+        one: "Jurassic Park",
+        two: "The Lost World: Jurassic Park", 
+        three: "Jurassic Park III"
+    },
+    second: {
+        four: "Jurassic World",
+        five: "Jurassic World: Fallen Kingdom",
+        six: "Jurassic World: Dominion"
+    }
+};
+
+//3b
+const seeJPMovies = function([{one, two, three}, {four, five, six}]){
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+};
+
+
+
+seeJPMovies(jurassicParkMovies);
+ 
+    
+ 
